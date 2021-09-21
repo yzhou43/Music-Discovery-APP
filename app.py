94d63949b11dc17c9b522ddcc326977b6179b3b2
yzhou43 <77255875+yzhou43@users.noreply.github.com>
2021-09-21 19:18:07 +0000

    work on html and css

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-app = flask.Flask(__name__)
-
 
 CLIENT_ID = 'a0e467772f0c481b9810cf2b5abc094a'
 
@@ -30,24 +28,28 @@ headers = {
     'Authorization': 'Bearer {token}'.format(token=access_token)
 }
 
-artists = ['06HL4z0CvFAxyc27GXpf02', '00FQb4jTyendYWaN8pK0wa',
+artists = ['06HL4z0CvFAxyc27GXpf02', '43ZHCT0cAZBISjO8DG9PnE',
            '6qqNVTkY8uBg9cP3Jd7DAH', '66CXWjxzNUsdJxJ2JdwvnR']
-# Get an Artist's Top Tracks
-rand_artist = random.randint(0,len(artists)-1)
-toptrack = requests.get('https://api.spotify.com/v1/artists/%s/top-tracks' %
-                        artists[rand_artist], headers=headers, params={'market': 'US'})
 
-toptrack_json = toptrack.json()
-print(toptrack.status_code)
-rand_song = random.randint(0,len(toptrack_json['tracks'])-1)
-song_info = toptrack_json['tracks'][rand_song]
-print(song_info)
+
+app = flask.Flask(__name__)
+
 
 @app.route("/")  # Python decorator
 def main():
-    
-    
-    return flask.render_template("index.html")
+    # Get an Artist's Top Tracks
+    rand_artist = random.randint(0, len(artists)-1)
+    toptrack = requests.get('https://api.spotify.com/v1/artists/%s/top-tracks' %
+                            artists[rand_artist], headers=headers, params={'market': 'US'})
+    toptrack_json = toptrack.json()
+    print(toptrack.status_code)
+    rand_song = random.randint(0, len(toptrack_json['tracks'])-1)
+    song_info = toptrack_json['tracks'][rand_song]
+    # print(song_info)
+    # its song name, song artist, song-related image, song preview URL
+    return flask.render_template("index.html", name=song_info['name'], artist=song_info['album']['artists'][0]['name'],
+                                 preview=song_info['preview_url'], img=song_info['album']['images'][0]['url'])
+
 
 app.run(
     debug=True
