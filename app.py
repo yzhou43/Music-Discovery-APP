6d2b8e569ebaf9ada8982dc070c69efaa1c38cde
yzhou43 <yzhou43@student.gsu.edu>
2021-10-12 15:53:59 -0400

    extract the get top tracks function to a seperate file

--- app.py
+++ app.py
@@ -1,62 +1,35 @@
 import flask
-import requests
 import os
-import random
 from dotenv import find_dotenv, load_dotenv
 from lyrics import genius
+from top_tracks import get_tracks
 
 load_dotenv(find_dotenv())
 
-AUTH_URL = 'https://accounts.spotify.com/api/token'
-
-# POST
-auth_response = requests.post(AUTH_URL, {
-    'grant_type': 'client_credentials',
-    'client_id': os.getenv("CLIENT_ID"),
-    'client_secret': os.getenv("CLIENT_SECRET")
-})
-
-# convert the response to JSON
-auth_response_data = auth_response.json()
-
-# save the access token
-access_token = auth_response_data['access_token']
-
-headers = {
-    'Authorization': 'Bearer {token}'.format(token=access_token)
-}
-
-artists = ['06HL4z0CvFAxyc27GXpf02', '43ZHCT0cAZBISjO8DG9PnE',
-           '41MozSoPIsD1dJM0CLPjZF']
+artists = ["06HL4z0CvFAxyc27GXpf02", "43ZHCT0cAZBISjO8DG9PnE", "41MozSoPIsD1dJM0CLPjZF"]
 
 app = flask.Flask(__name__)
-
 
 @app.route("/")  # Python decorator
 def main():
     # Get an Artist's Top Tracks
-    rand_artist = random.randint(0, len(artists)-1)
-    try:
-        toptrack = requests.get('https://api.spotify.com/v1/artists/%s/top-tracks' %
-                                artists[rand_artist], headers=headers, params={'market': 'US'})
-        toptrack_json = toptrack.json()
-        rand_song = random.randint(0, len(toptrack_json['tracks'])-1)
-        song_info = toptrack_json['tracks'][rand_song]
-        name = song_info['name']
-    except:
-          return flask.render_template("error.html", error_state='Failed to get the song\'s infomation!') 
-    
+    song_info = get_tracks(artists)
+    name = song_info["name"]
+
     # use the genius API
     lyrics_url, artist_img, artist_url, name_genius = genius(name)
     # its song name, song artist, song-related image, song preview URL
-    return flask.render_template("index.html", name=name, artist=song_info['album']['artists'][0]['name'],
-                                 preview=song_info['preview_url'], img=song_info['album']['images'][0]['url'],
-                                lyrics_url=lyrics_url, artist_img=artist_img, artist_url=artist_url,
-                                name_genius=name_genius)
+    return flask.render_template(
+        "index.html",
+        name=name,
+        artist=song_info["album"]["artists"][0]["name"],
+        preview=song_info["preview_url"],
+        img=song_info["album"]["images"][0]["url"],
+        lyrics_url=lyrics_url,
+        artist_img=artist_img,
+        artist_url=artist_url,
+        name_genius=name_genius,
+    )
 
 
-app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', 8080)),
-    debug=True
-)
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True)
---
+++ top_tracks.py
@@ -0,0 +1,45 @@
+import flask
+import requests
+import os
+import random
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+AUTH_URL = "https://accounts.spotify.com/api/token"
+
+# POST
+auth_response = requests.post(
+    AUTH_URL,
+    {
+        "grant_type": "client_credentials",
+        "client_id": os.getenv("CLIENT_ID"),
+        "client_secret": os.getenv("CLIENT_SECRET"),
+    },
+)
+
+# convert the response to JSON
+auth_response_data = auth_response.json()
+
+# save the access token
+access_token = auth_response_data["access_token"]
+
+headers = {"Authorization": "Bearer {token}".format(token=access_token)}
+
+def get_tracks(artists):
+    # Get an Artist's Top Tracks
+    rand_artist = random.randint(0, len(artists) - 1)
+    try:
+        toptrack = requests.get(
+            "https://api.spotify.com/v1/artists/%s/top-tracks" % artists[rand_artist],
+            headers=headers,
+            params={"market": "US"},
+        )
+        toptrack_json = toptrack.json()
+        rand_song = random.randint(0, len(toptrack_json["tracks"]) - 1)
+        song_info = toptrack_json["tracks"][rand_song]
+    except:
+        return flask.render_template(
+            "error.html", error_state="Failed to get the song's infomation!"
+        )
+    return song_info
