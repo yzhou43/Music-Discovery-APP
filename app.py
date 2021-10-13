dff054c20ae18e1044a66c1c8fc9313f4e36c937
yzhou43 <yzhou43@student.gsu.edu>
2021-10-13 01:46:09 -0400

    create the login system

--- app.py
+++ app.py
@@ -3,15 +3,82 @@ import os
 from dotenv import find_dotenv, load_dotenv
 from lyrics import genius
 from top_tracks import get_tracks
+from form_fields import *
+from flask_login import LoginManager, login_user, current_user, logout_user
+from form_fields import *
+from models import *
 
 load_dotenv(find_dotenv())
 
 artists = ["06HL4z0CvFAxyc27GXpf02", "43ZHCT0cAZBISjO8DG9PnE", "41MozSoPIsD1dJM0CLPjZF"]
 
 app = flask.Flask(__name__)
+app.secret_key=os.environ.get('SECRET')
 
-@app.route("/")  # Python decorator
-def main():
+# Configure database
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
+db = SQLAlchemy(app)
+
+# Configure flask login
+login = LoginManager(app)
+login.init_app(app)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+# The sign up page
+@app.route("/", methods=['GET', 'POST'])
+def signup():
+
+    signup_form = SignupForm()
+
+    # Check if the sign up is successful
+    if signup_form.validate_on_submit():
+        username = signup_form.username.data
+        password = signup_form.password.data
+
+        # Add the username and password to the database
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+        flask.flash('You were successfully signed up! Please log in.')
+        return flask.redirect(flask.url_for('login'))
+
+    return flask.render_template("signup.html", form=signup_form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+
+    login_form = LoginForm()
+
+    # Check if the login is successful
+    if login_form.validate_on_submit():
+        user_object = User.query.filter_by(username=login_form.username.data).first()
+        login_user(user_object)
+        if current_user.is_authenticated:
+            return current_user.username
+            # return flask.redirect(flask.url_for('index'))
+        else:
+            return "Failed to log in"
+
+    return flask.render_template("login.html", form=login_form)
+
+# User log out
+@app.route("/logout", methods=['GET'])
+def logout():
+
+    logout_user()
+    flask.flash('You have logged out successfully')
+    return flask.redirect(flask.url_for('login'))
+    
+
+
+
+@app.route("/index")
+def index():
     # Get an Artist's Top Tracks
     song_info = get_tracks(artists)
     name = song_info["name"]
---
+++ form_fields.py
@@ -0,0 +1,52 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, ValidationError
+
+from models import User
+
+# Check the login input
+def check_login(form, field):
+
+    password = field.data
+    username = form.username.data
+
+    # Check if the username is in the database
+    user_data = User.query.filter_by(username=username).first()
+    if user_data is None:
+        raise ValidationError("Username or password is incorrect")
+    # Check password in invalid
+    elif password != user_data.password:
+        raise ValidationError("Username or password is incorrect")
+
+# Check if the username already exists
+def check_username(form, field):
+    username = field.data
+    user_object = User.query.filter_by(username=username).first()
+    if user_object:
+        raise ValidationError(
+            "Username already exists! Please choose a different one."
+        )
+
+class SignupForm(FlaskForm):
+
+    username = StringField(
+        "username",
+        validators=[InputRequired(), check_username],
+    )
+    password = PasswordField(
+        "password",
+        validators=[InputRequired()],
+    )
+    submit_button = SubmitField("Sign up")
+
+
+class LoginForm(FlaskForm):
+
+    username = StringField(
+        "username", validators=[InputRequired()]
+    )
+    password = PasswordField(
+        "password",
+        validators=[InputRequired(), check_login],
+    )
+    submit_button = SubmitField("Login")
